7a7ffc1592edbc4514b7f5d10a097bc0cf2a3d9b
Kishan chandravadiya <kishanchandravadiya946@gmail.com>
2024-10-19 23:10:36 +0530

    Add project

--- NEWS_App/obj/Debug/net8.0/NEWS_App.AssemblyInfo.cs
+++ NEWS_App/obj/Debug/net8.0/NEWS_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NEWS_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e10f8a1ab84c01e9336c03c43bb9ae2b1fd2ddc")]
 [assembly: System.Reflection.AssemblyProductAttribute("NEWS_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NEWS_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
